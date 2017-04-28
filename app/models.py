c5899cae063491d9aa4dca144e0858e87ff1e76b
fastfuse <banderstadt251@gmail.com>
2017-04-28 22:25:20 +0300

    token stuff finished

--- app/models.py
+++ app/models.py
@@ -56,7 +56,7 @@ class User(db.Model):
         """
         try:
             payload = {
-                'exp': datetime.utcnow() + timedelta(days=0, seconds=5),
+                'exp': datetime.utcnow() + timedelta(days=1),
                 'iat': datetime.utcnow(),
                 'sub': user_id
             }
@@ -72,14 +72,43 @@ class User(db.Model):
     @staticmethod
     def decode_auth_token(auth_token):
         """
-        Decodes the auth token
+        Validates the auth token
         :param auth_token:
         :return: integer|string
         """
         try:
             payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
-            return payload['sub']
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
         except jwt.ExpiredSignatureError:
             return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
             return 'Invalid token. Please log in again.'
+
+
+class BlacklistToken(db.Model):
+    """ Token Model for storing blacklisted JWT tokens """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token=''):
+        self.token = token
+        self.blacklisted_on = datetime.now()
+
+    def __repr__(self):
+        return '<Token: {}>'.format(self.token)
+
+    @staticmethod
+    def check_blacklist(auth_token):
+        # check whether auth token has been blacklisted
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        if res:
+            return True
+        else:
+            return False
--- app/views.py
+++ app/views.py
@@ -2,32 +2,37 @@
 # -*- coding: utf-8 -*-
 
 import copy
-from app import app, db, admin, models
+from app import app, db, admin, models, bcrypt
 from flask import (request,
                    redirect,
                    render_template,
                    make_response,
                    url_for,
-                   json)
+                   jsonify)
 from flask.views import MethodView
 from datetime import datetime
 from flask_admin.contrib.sqla import ModelView
 from wtforms import fields
+from collections import namedtuple
 
 
 # ===========================  Help functions   ==============================
 
 def register_api(view, endpoint, url, pk='id', pk_type='any'):
-    """ Function to simplify registration of API views (based on MethodView) """
+    """ Function to simplify registration
+        of API Views (based on MethodView) """
     view_func = view.as_view(endpoint)
     app.add_url_rule(url, defaults={pk: None},
                      view_func=view_func, methods=['GET',])
     app.add_url_rule(url, view_func=view_func, methods=['POST',])
-
     app.add_url_rule('{url}<{pk_type}:{pk}>'.format(url=url,
                                                     pk_type=pk_type,
                                                     pk=pk),
                      view_func=view_func, methods=['GET', 'PUT', 'DELETE'])
+
+
+# namedtuple to simplify creation of response messages
+Response = namedtuple('Response', ['status', 'message'])
 
 
 # ============================   Admin views   ================================
@@ -48,6 +53,7 @@ class UserView(ModelView):
 
 admin.add_view(IncidentsView(models.Incident, db.session))
 admin.add_view(UserView(models.User, db.session))
+admin.add_view(ModelView(models.BlacklistToken, db.session))
 
 
 # =============================   Index page   ==============================
@@ -61,7 +67,7 @@ def index():
 # ==============================   API   ====================================
 
 class IncidentsAPI(MethodView):
-
+    """ Incidents Resourse """
     def get(self, incident_id):
         """ Get all incidents or single incident by ID """
 
@@ -71,15 +77,10 @@ class IncidentsAPI(MethodView):
         if incident_id:
             incident_query = models.Incident.query.get_or_404(incident_id)
 
-            incident = {}
-            incident['id'] = incident_query.id
-            incident['title'] = incident_query.title
-            incident['description'] = incident_query.description
-            incident['location'] = incident_query.location
-            incident['timestamp'] = incident_query.timestamp
-            incident['status'] = incident_query.status
+            incident = incident_query.__dict__
+            incident.pop('_sa_instance_state')
 
-            return make_response(json.jsonify(incident))
+            return make_response(jsonify(incident))
 
         else:
             status = request.args.get('status', None)
@@ -92,16 +93,11 @@ class IncidentsAPI(MethodView):
 
             incidents = []
             for item in incidents_query:
-                incident = {}
-                incident['id'] = item.id
-                incident['title'] = item.title
-                incident['description'] = item.description
-                incident['location'] = item.location
-                incident['timestamp'] = item.timestamp
-                incident['status'] = item.status
+                incident = item.__dict__
+                incident.pop('_sa_instance_state')
                 incidents.append(incident)
 
-            return make_response(json.jsonify(incidents=incidents,
+            return make_response(jsonify(incidents=incidents,
                                               count=len(incidents)))
 
     def post(self):
@@ -117,7 +113,7 @@ class IncidentsAPI(MethodView):
         db.session.add(new_incident)
         db.session.commit()
 
-        return make_response(json.jsonify(response), 201)
+        return make_response(jsonify(response), 201)
 
 
 class RegistrationAPI(MethodView):
@@ -144,30 +140,124 @@ class RegistrationAPI(MethodView):
                     'auth_token': auth_token.decode()
                 }
                 print(user.__dict__)
-                return make_response(json.jsonify(response_object)), 201
+                return make_response(jsonify(response_object)), 201
 
             except Exception as e:
-                response_object = {
-                    'status': 'Fail',
-                    'message': 'Some error occurred. Please try again.'
-                }
-                return make_response(json.jsonify(response_object)), 401
+                response = Response('Fail',
+                                    'Some error occurred. Please try again.')
+                return make_response(jsonify(response._asdict())), 401
 
         else:
-            response_object = {
-                'status': 'Fail',
-                'message': 'User already exists. Please Log in.',
-            }
-
-            return make_response(json.jsonify(response_object)), 202
+            response = Response('Fail', 'User already exists. Please Log in.')
+            return make_response(jsonify(response._asdict())), 202
 
 
+class LoginAPI(MethodView):
+    """ User Login Resource """
+    def post(self):
+        # get the post data
+        post_data = request.get_json()
+        try:
+            # fetch the user data
+            user = models.User.query.filter_by(
+                username=post_data.get('username')).first()
 
+            if user and bcrypt.check_password_hash(
+                user.password, post_data.get('password')
+            ):
+                auth_token = user.encode_auth_token(user.id)
+                if auth_token:
+                    response = {
+                        'status': 'Success',
+                        'message': 'Successfully logged in.',
+                        'auth_token': auth_token.decode()
+                    }
+                    return make_response(jsonify(response)), 200
+
+            else:
+                response = Response('Fail', 'User does not exist.')
+                return make_response(jsonify(response._asdict())), 404
+
+        except Exception as e:
+            print(e)
+            response = Response('Fail', 'Try again')
+            return make_response(jsonify(response._asdict())), 500
+
+
+class UserAPI(MethodView):
+    """ User Resource """
+    def get(self, id):
+        # get the auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split()[1]
+        else:
+            auth_token = ''
+
+        if auth_token:
+            resp = models.User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                user = models.User.query.filter_by(id=resp).first()
+                response_object = {
+                    'status': 'Success',
+                    'data': {
+                        'user_id': user.id,
+                        'username': user.username,
+                        'admin': user.admin,
+                        'registered_on': user.registered_on
+                    }
+                }
+                return make_response(jsonify(response_object)), 200
+
+            response = Response('Fail', resp)
+            return make_response(jsonify(response._asdict())), 401
+
+        else:
+            response = Response('Fail', 'Provide a valid auth token')
+            return make_response(jsonify(response._asdict())), 401
+
+
+class LogoutAPI(MethodView):
+    """ Logout Resource """
+    def post(self):
+        # get auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+
+        if auth_token:
+            resp = models.User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                # mark the token as blacklisted
+                blacklist_token = models.BlacklistToken(token=auth_token)
+                try:
+                    # insert the token
+                    db.session.add(blacklist_token)
+                    db.session.commit()
+
+                    response = Response('Success', 'Successfully logged out.')
+                    return make_response(jsonify(response._asdict())), 200
+
+                except Exception as e:
+                    response = Response('Fail', e)
+                    return make_response(jsonify(response._asdict())), 200
+
+            else:
+                response = Response('Fail', resp)
+                return make_response(jsonify(response._asdict())), 401
+
+        else:
+            response = Response('Fail', 'Provide a valid auth token.')
+            return make_response(jsonify(response._asdict())), 403
 
 
 # =====================   Register API endpoints   ==========================
 
 register_api(IncidentsAPI, 'incidents_api', '/api/incidents/',
              pk='incident_id', pk_type='int')
-
-register_api(RegistrationAPI, 'registration_api', '/api/register') # /api/auth ?
+register_api(RegistrationAPI, 'registration_api', '/api/register')
+register_api(LoginAPI, 'login_api', '/api/login')
+register_api(UserAPI, 'user_api', '/api/user')
+register_api(LogoutAPI, 'logout_api', '/api/logout')
